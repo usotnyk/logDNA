8eb3738272d9626e437cdbb6ae9e7eb09a72e3c8
Julia Sotnyk <usotnyk@gmail.com>
2019-02-09 18:33:47 -0500

    Created workspace tabs with style

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var contentData = [
+  {
+    workspaceHref: "workspace1",
+  },
+  {
+    workspaceHref: "workspace2",
+  },
+  {
+    workspaceHref: "workspace3",
+  },
+  {
+    workspaceHref: "workspace4",
+  }
+]
+
+$(document).ready(function() {
+  console.log("ready")
+})
